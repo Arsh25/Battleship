c5d545ba3bbe252d1ff59c5c5d139d8cb1163fc0
Arsh Chauhan <achauhan@alaska.edu>
2016-02-24 18:19:00 -0900

    Added more tests for Board and cell functionality

--- Battleship_test.cpp
+++ Battleship_test.cpp
@@ -8,6 +8,7 @@
 #include "catch.hpp"       // For the "Catch" unit-testing framework
 
 #include "Board.h"
+#include "Cell.h"
 
 #include <vector>
 using std::vector;
@@ -29,16 +30,46 @@ using namespace std::placeholders;
 
 TEST_CASE("2 param  Constructor", "[Board]")
 {
-	Board primary(10,true);
+	const int SIZE = 10;
+	const int CELLS = SIZE*SIZE;
+	Board primary(SIZE,true);
 
-	REQUIRE(primary.getSize() == 10);
+	REQUIRE(primary.getSize() == SIZE);
 	REQUIRE(primary.isPrimary() == true);
 
-	Board target(10,false); //Testing Target board construction
-	REQUIRE(target.getSize() == 10);
+	Board target(SIZE,false); //Testing Target board construction
+	REQUIRE(target.getSize() == SIZE);
 	REQUIRE(target.isPrimary() == false);
 
+}
 
+TEST_CASE(" Testing manipulating cells from board","[BOARD]")
+{
+	const int SIZE = 10;
+	const int CELLS = SIZE*SIZE;
+	Board cellTest(SIZE,true);
+	
+	REQUIRE(cellTest.isPrimary() == true);
+	REQUIRE(cellTest.totalCells() == CELLS);
+	cellTest.setHead(10);
+	REQUIRE(cellTest.isHead(10) == true);
+	cellTest.setOccupied(10);
+	REQUIRE(cellTest.isOccupied(10) == true);
+
+}
+
+//Tests to see if cell properties are correctly returned
+TEST_CASE("Setting Cell Properties", "[CELL]")
+{
+	Cell cell1;
+	REQUIRE(cell1.isOccupied() == false);
+	cell1.setOccupied();
+	REQUIRE(cell1.isOccupied() == true);
+
+	REQUIRE(cell1.isHead() == false);
+	cell1.setHead();
+	REQUIRE(cell1.isHead() == true);
+	
 }
 
 
